1642e09220ddbf1ba71889b02e574dffb5736010
Nicholas Dallamora <ndallamora@edgecast.com>
2018-09-19 12:32:39 -0700

    updated security group and added hostname to hello world

--- install_apache.sh
+++ install_apache.sh
@@ -5,5 +5,5 @@ sudo apt-get install apache2 -y
 sudo systemctl enable apache2
 sudo systemctl start apache2
 sudo chmod 777 /var/www/html/index.html
-sudo echo 'hello world' > /var/www/html/index.html
+sudo echo 'hello world. i am' $HOSTNAME > /var/www/html/index.html
 sudo systemctl restart apache2
--- onica_net.tf
+++ onica_net.tf
@@ -1,3 +1,14 @@
+resource "aws_security_group" "elb" {
+  name = "onica-elb"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "instance" {
   name = "onica-security-group"
 
